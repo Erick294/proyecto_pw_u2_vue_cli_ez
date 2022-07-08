97a661e5bed984b3b6d6f213cf60c45d6d04fabb
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2022-07-07 19:43:35 -0500

    Taller 14

---
+++ src/components/Contador.vue
@@ -0,0 +1,59 @@
+<template>
+  <h2>Contador!!!!!</h2>
+  <p>{{numero}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{numero}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{numero}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{numero}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{numero}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+
+  <div>
+      <button v-on:click="incrementar">Sumar 1</button>
+      <button v-on:click="decrementar">Restar 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Contador",
+    data(){
+        return{
+            numero: 5
+        }
+    },
+    methods:{
+        obtenerCuadrado(){
+            console.log("Esta dentro del calculo cuadrado")
+            return this.numero * this.numero
+        },
+        incrementar(){
+            return this.numero++
+        },
+        decrementar(){
+            return this.numero--
+        }
+    }, 
+    //Computado se declara como un metodo normal, se ejecuta una vez si se encuentra duplicado
+    //Computado no puede tener parámetros
+    computed:{
+        obtenerCuadradoComputado(){
+            console.log("Aqui entro cuadrado computado")
+            return this.numero * this.numero
+        } 
+    }
+}
+</script>
+
+<style>
+button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    color: white;
+    margin: 0px 5px;
+    cursor: pointer;
+}
+
+button:hover{
+    background-color:darkslategray;
+    transition: 0.9s ease-in-out;
+}
+</style>
